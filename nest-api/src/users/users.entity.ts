c3be2755e65909acaad9b86976d18cbf53d75fc2
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-07-24 19:58:15 +0200

    Add datetime columns to users

--- nest-api/src/users/users.entity.ts
+++ nest-api/src/users/users.entity.ts
@@ -1,4 +1,10 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity('users')
 export class User {
@@ -16,4 +22,10 @@ export class User {
 
   @Column({ default: true })
   isActive: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
 }
