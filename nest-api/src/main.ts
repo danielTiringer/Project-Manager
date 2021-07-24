32cee97caee26fbd0caf1028ea0d9c51671fff72
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-07-24 21:01:19 +0200

    Add cookie parser package

--- nest-api/src/main.ts
+++ nest-api/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(cookieParser());
+
+  app.enableCors({
+    credentials: true,
+    origin: 'http://localhost:3001',
+  });
+
   await app.listen(process.env.API_PORT);
 }
+
 bootstrap();
