11c92a0f64f9b0e85e19005b56139b3a2e1658db
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-07-24 17:44:54 +0200

    Set up typeorm module connection

--- nest-api/src/app.module.ts
+++ nest-api/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'database',
+      port: parseInt(process.env.MYSQL_PORT),
+      username: 'root',
+      password: process.env.MYSQL_ROOT_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
