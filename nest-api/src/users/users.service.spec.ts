ac878f1cd13a58fba34cea57d3f322baaac0ff22
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-07-25 09:38:05 +0200

    Write test for usersservice's findone function

---
+++ nest-api/src/users/users.service.spec.ts
@@ -0,0 +1,44 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './users.entity';
+import { UsersService } from './users.service';
+
+const oneUser = new User('Test User', 'test@example.com', 'password');
+
+describe('UsersService', () => {
+  let usersService: UsersService;
+  let repository: Repository<User>;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: {
+            findOne: jest.fn().mockResolvedValue(oneUser),
+            create: jest.fn().mockResolvedValue(oneUser),
+          },
+        },
+      ],
+    }).compile();
+
+    usersService = module.get<UsersService>(UsersService);
+    repository = module.get<Repository<User>>(getRepositoryToken(User));
+  });
+
+  it('should be defined', () => {
+    expect(usersService).toBeDefined();
+  });
+
+  describe('findOne', () => {
+    it('should get a single user', () => {
+      const repositorySpy = jest.spyOn(repository, 'findOne');
+      expect(
+        usersService.findOne({ email: 'test@example.com' }),
+      ).resolves.toEqual(oneUser);
+      expect(repositorySpy).toBeCalledWith({ email: 'test@example.com' });
+    });
+  });
+});
