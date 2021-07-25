53a11427f90a2d7b7e90cbcee979f90588fdfb3c
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-07-25 22:39:13 +0200

    Take out lowercasing temporarily

--- nest-api/src/user/models/user.entity.ts
+++ nest-api/src/user/models/user.entity.ts
@@ -30,10 +30,10 @@ export class User {
   @UpdateDateColumn({ select: false })
   updated_at?: Date;
 
-  @BeforeInsert()
-  emailToLowerCase() {
-    this.email = this.email.toLowerCase();
-  }
+  // @BeforeInsert()
+  // emailToLowerCase() {
+  //   this.email = this.email.toLowerCase();
+  // }
 
   constructor(
     name: string,
