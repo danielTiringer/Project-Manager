346e1c27b1a1c12f3f78d4562213e4995dc4f2e3
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-08-01 19:22:35 +0200

    Use authmodule in main

--- nest-api/src/auth/auth.module.ts
+++ nest-api/src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from 'src/user/user.module';
+import { PassportModule } from '@nestjs/passport';
+import { UserModule } from '../user/user.module';
 import { AuthService } from './services/auth.service';
+import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
-  imports: [UserModule],
-  providers: [AuthService],
+  imports: [UserModule, PassportModule],
+  providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}
