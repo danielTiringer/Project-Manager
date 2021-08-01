be2516c20001ac541096de4e19d1b2e8f6cfa0f7
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-08-01 20:32:00 +0200

    Register serializer

--- nest-api/src/auth/auth.module.ts
+++ nest-api/src/auth/auth.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { UserModule } from '../user/user.module';
+import { SessionSerializer } from './serializers/session.serializer';
 import { AuthService } from './services/auth.service';
 import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
-  imports: [UserModule, PassportModule],
-  providers: [AuthService, LocalStrategy],
+  imports: [UserModule, PassportModule.register({ session: true })],
+  providers: [AuthService, LocalStrategy, SessionSerializer],
 })
 export class AuthModule {}
