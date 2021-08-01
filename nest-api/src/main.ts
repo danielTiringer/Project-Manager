6e22de828225e013863ffccfcf13e3d4d74e2d8b
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-08-01 20:15:46 +0200

    Implement redis connection

--- nest-api/src/main.ts
+++ nest-api/src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import * as session from 'express-session';
+import * as redis from 'redis';
+import * as connectRedis from 'connect-redis';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +16,25 @@ async function bootstrap() {
     credentials: true,
     origin: 'http://localhost:3001',
   });
+
+  const redisClient = redis.createClient(
+    parseInt(process.env.REDIS_PORT),
+    'redis',
+  );
+
+  const RedisStore = connectRedis(session);
+
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
+      cookie: { maxAge: 3600000 },
+      store: new RedisStore({
+        client: redisClient,
+      }),
+    }),
+  );
 
   await app.listen(process.env.API_PORT);
 }
