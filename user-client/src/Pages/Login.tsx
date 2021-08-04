79529713eb7ef8a2745a55d447e0656ddbde0e97
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-08-04 08:32:11 +0200

    Implement login functionality

--- user-client/src/Pages/Login.tsx
+++ user-client/src/Pages/Login.tsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 
 export default function Login(): JSX.Element {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const login = () => {
+    axios
+      .post(
+        'http://localhost:3001/api/login',
+        { email, password },
+        { withCredentials: true },
+      )
+      .then((res) => {
+        console.log(res.data);
+      });
+  };
+
   return (
     <div>
-      <p>Login Page</p>
+      <h1>Login</h1>
+      <input
+        type="text"
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={login}>Login</button>
     </div>
   );
 }
