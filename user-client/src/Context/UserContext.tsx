817b415aa6fa87d45c779058d7cfcc90ac8f4800
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-08-05 20:27:59 +0200

    Set user in state inside context

--- user-client/src/Context/UserContext.tsx
+++ user-client/src/Context/UserContext.tsx
@@ -1,11 +1,29 @@
-import React, { createContext, PropsWithChildren } from 'react';
+import React, {
+  createContext,
+  PropsWithChildren,
+  useEffect,
+  useState,
+} from 'react';
+import axios from 'axios';
 
 export const userContext = createContext<any>({});
 
 export default function UserContext(
   props: PropsWithChildren<any>,
 ): JSX.Element {
+  const [user, setUser] = useState<any>();
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/api/user', {
+        withCredentials: true,
+      })
+      .then((res) => {
+        setUser(res.data);
+      });
+  }, []);
+
   return (
-    <userContext.Provider value={1000}>{props.children}</userContext.Provider>
+    <userContext.Provider value={user}>{props.children}</userContext.Provider>
   );
 }
