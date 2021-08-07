f42ea6c58f2b2849f7fcc05f440a51b565e1bea7
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-08-07 15:38:46 +0200

    Implement register functionality

--- user-client/src/Pages/Register.tsx
+++ user-client/src/Pages/Register.tsx
@@ -1,9 +1,56 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 
+/* eslint-disable max-lines-per-function */
 export default function Register(): JSX.Element {
+  const [name, setName] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [passwordRepeat, setPasswordRepeat] = useState<string>('');
+
+  const register = () => {
+    if (password !== passwordRepeat) {
+      console.log('Password and repeat do not match');
+      return;
+    }
+
+    axios
+      .post(
+        'http://localhost:3001/api/login',
+        { name, email, password },
+        { withCredentials: true },
+      )
+      .then((res) => {
+        if (res.status === 201) {
+          window.location.href = '/login';
+        }
+      });
+  };
+
   return (
     <div>
-      <p>Register</p>
+      <h1>Register</h1>
+      <input
+        type="text"
+        placeholder="Name"
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Password again"
+        onChange={(e) => setPasswordRepeat(e.target.value)}
+      />
+      <button onClick={register}>Register</button>
     </div>
   );
 }
