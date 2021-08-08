60d2327680d452cf17410ccf904191bf38c5a7ce
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-08-08 11:03:21 +0200

    Make a delete function

--- user-client/src/Pages/Profile.tsx
+++ user-client/src/Pages/Profile.tsx
@@ -1,6 +1,18 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useContext } from 'react';
+import { userContext } from '../Context/UserContext';
 
 export default function Profile(): JSX.Element {
+  const ctx = useContext(userContext);
+
+  const deleteUser = () => {
+    if (ctx) {
+      axios.post('http://localhost:3001/api/user', {
+        id: ctx?.user?.id,
+      });
+    }
+  };
+
   return (
     <div>
       <p>Profile Page</p>
