6f450b1a1374be2a57c734dc9c9c7fe6d31442f3
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-10-21 23:33:35 +0200

    Temporarily take out eslint

--- user-client/src/Pages/Profile.tsx
+++ user-client/src/Pages/Profile.tsx
@@ -5,6 +5,7 @@ import { userContext } from '../Context/UserContext';
 export default function Profile(): JSX.Element {
   const ctx = useContext(userContext);
 
+  // eslint-disable-next-line
   const deleteUser = () => {
     if (ctx) {
       axios.post('http://localhost:3001/api/user', {
